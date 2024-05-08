ff4643933c77d4f070ac445c5edc82299de3752b
Tim Schupp <tim@timschupp.de>
2024-05-08 13:02:38 +0200

    constants may accept dynamic env from vike for pre-rendering

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,22 @@
+// Vike allows bundeling env params or dynamicly loading them
+// In order to assure this also works in the packages version we provide sensible defaults, that can be overwritten by the bundler
+
+function metaOrDefault(meta: any, defaultValue: any) {
+  try {
+    if (typeof meta === "function") {
+      meta = meta()
+    }
+  } catch (e) {
+    console.error(e)
+    meta = ""
+  }
+  return (meta && (meta !== "")) ? meta : defaultValue
+}
+
 export const LOGIN_AS_GUEST = {
-  allowed: false,
-  username: "",
-  password: ""
+  allowed: metaOrDefault(() => { return import.meta.env.PUBLIC_ENV__GUEST_LOGIN_ALLOWED }, true),
+  username: metaOrDefault(() => { return import.meta.env.PUBLIC_ENV__GUEST_USERNAME }, "guest"),
+  password: metaOrDefault(() => { return import.meta.env.PUBLIC_ENV__GUEST_PASSWORD }, "guest"),
 }
 
 export enum BT {
@@ -9,20 +24,20 @@ export enum BT {
   DOCS = "docs",
   WEB = "web",
 }
-export const BUILD_TYPE: BT = BT.WEB;
+export const BUILD_TYPE: BT = metaOrDefault(() => { return import.meta.env.PUBLIC_ENV__BUILD_TYPE }, BT.WEB);
 export const STATIC_EXPORT = BUILD_TYPE !== BT.WEB;
-export const LOGIN_ROUTE = "/login";
+export const LOGIN_ROUTE = metaOrDefault(() => { return import.meta.env.PUBLIC_ENV__LOGIN_ROUTE }, "/login");
 
 // 'ROUTE_PREFIX' is added on 'navigate' and 'Link' from `@/components/atoms/Link`
-export const ROUTE_PREFIX = ""
+export const ROUTE_PREFIX = metaOrDefault(() => { return import.meta.env.PUBLIC_ENV__ROUTE_PREFIX }, "");
 
 // 'FRONTNED_BACKEND_ROUTE' is used for client side requests
-export const FRONTNED_BACKEND_ROUTE = ""
+export const FRONTNED_BACKEND_ROUTE = metaOrDefault(() => { return import.meta.env.PUBLIC_ENV__FRONTNED_BACKEND_ROUTE }, "");
 
 // 'WEBSOCKET_URL' constructed client websocket url
-export const WEBSOCKET_PROTOCOLL = "ws://";
-export const WEBSOCKET_HOST = "localhost:8000";
-export const WEBSOCKET_PATH = "/ws/chat/";
+export const WEBSOCKET_PROTOCOLL = metaOrDefault(() => { return import.meta.env.PUBLIC_ENV__WEBSOCKET_PROTOCOLL }, "ws://");
+export const WEBSOCKET_HOST = metaOrDefault(() => { return import.meta.env.PUBLIC_ENV__WEBSOCKET_HOST }, "localhost:8000");
+export const WEBSOCKET_PATH = metaOrDefault(() => { return import.meta.env.PUBLIC_ENV__WEBSOCKET_PATH }, "/ws/chat/");
 export const WEBSOCKET_URL = `${WEBSOCKET_PROTOCOLL}${WEBSOCKET_HOST}${WEBSOCKET_PATH}`;
 
-export const BASE_PAGE_TITLE = "Open Chat Interface"
+export const BASE_PAGE_TITLE = metaOrDefault(() => { return import.meta.env.PUBLIC_ENV__BASE_PAGE_TITLE }, "Open Chat Interface")
