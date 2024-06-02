1a9d2c4b9ac4edec3a439d4057bbb9ce7ea3314e
Tim Schupp <tim@timschupp.de>
2024-06-03 00:59:59 +0200

    improved interaction button

--- src/audio_chat/ToggleRecordingButton.tsx
+++ src/audio_chat/ToggleRecordingButton.tsx
@@ -1,14 +1,23 @@
 import { cn } from '../lib/utils';
 
 export function ToggleRecordingButton({
+    audioState,
     isRecording,
     setIsRecording
 }) {
     // Implementation here...
-    return <div className={cn('flex h-[80px] hover:h-[70px] w-[80px] hover:w-[70px] hover:m-[5px] rounded-full bg-base-200 content-center justify-center items-center', {
-        'bg-warning': isRecording
-    })}
-        onClick={() => setIsRecording(!isRecording)}>
-        {isRecording ? 'Stop' : 'Start'}
-    </div>
+    console.log("audioState", audioState)
+    if (['booted', 'listening'].includes(audioState)) {
+        return <div className={cn('flex h-[80px] hover:h-[70px] w-[80px] hover:w-[70px] hover:m-[5px] rounded-full bg-base-200 content-center justify-center items-center', {
+            'bg-warning': isRecording
+        })}
+            onClick={() => setIsRecording(!isRecording)}>
+            {isRecording ? 'Stop' : 'Start'}
+        </div>
+    } else if (['thinking', 'speaking'].includes(audioState)) {
+        return <div className={cn('flex h-[80px] hover:h-[70px] w-[80px] hover:w-[70px] hover:m-[5px] rounded-full bg-base-200 content-center justify-center items-center bg-error')}
+            onClick={() => { }}>
+            Interrupt
+        </ div>
+    }
 }
