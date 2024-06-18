f2a5ada2cc1bc6139747962696b6d9f75306529f
Tim Schupp <tim@timschupp.de>
2024-06-18 23:58:44 +0200

    landing page now mobile compatible

--- src/sections/LandingHero.tsx
+++ src/sections/LandingHero.tsx
@@ -26,37 +26,44 @@ const TEXTS = [{
 export function LandingHero() {
 
     const { logoUrl } = useContext(GlobalContext)
-    return <div className="flex flex-row items-center justify-center content-center h-screen">
-        <div className="flex flex-col flex-grow items-center justify-center content-center bg-base-200 w-2/3 h-full shadow-xl z-10">
-            <div className="flex w-full p-4 font-bold text-2xl">
+    return <>
+        <div className="relative w-full z-40">
+            <div className="absolute flex w-full p-4 font-bold text-2xl w-full">
                 Open-Chat
-                <div className="text-sm text-gray-500 mt-3 ml-1">by Msgmate.io</div>
-            </div>
-            <Typewriter
-                typingSpeed={10}
-                texts={TEXTS} />
-        </div>
-        <div className="flex flex-col flex-grow items-center justify-center content-center bg-base-100 w-1/3 h-full">
-            <div className="flex flex-col items-center justify-center content-center w-full flex-grow">
-                <CinematicLogo className={"mr-8"} size={320} />
-                <div className="flex flex-row items-center justify-center content-center w-full relative">
-                    <div className="flex w-1/2 justify-end pr-4">
-                        <Button variant="ghost" className="rounded-full border">Sign-up</Button>
-                    </div>
-                    <div className="flex w-1/2 justify-start pl-4">
-                        <Button variant="ghost" className="rounded-full border">Log-in</Button>
-                    </div>
-                </div>
-            </div>
-            <div className="flex flex-row items-center justify-center content-center w-full p-2">
-                <div className="flex w-1/2 justify-end">
-                    <Button variant="ghost" className="h-5 p-1 hover:text-neutral">Therms of use</Button>
-                </div>
-                |
-                <div className="flex w-1/2 justify-start">
-                    <Button variant="ghost" className="h-5 p-1">Privacy policy</Button>
-                </div>
+                <div className="text-sm text-gray-500 mt-3 ml-1">by Msgmate.io (beta)</div>
             </div>
         </div>
-    </div>
+        <div className="flex flex-row items-center justify-center content-center h-screen">
+            <div className="flex flex-col flex-grow items-center justify-center content-center bg-base-200 w-2/3 h-full shadow-xl z-10 hidden lg:flex relative">
+                <Typewriter
+                    typingSpeed={10}
+                    texts={TEXTS} />
+            </div>
+            <div className="flex flex-col flex-grow items-center justify-center content-center bg-base-100 w-1/3 h-full">
+                <div className="flex flex-col items-center justify-center content-center w-full flex-grow">
+                    <CinematicLogo className={"mr-8"} size={320} />
+                    <div className="flex flex-row items-center justify-center content-center w-full relative">
+                        <div className="flex w-1/2 justify-end pr-4">
+                            <Button variant="ghost" className="rounded-full border">Sign-up</Button>
+                        </div>
+                        <div className="flex w-1/2 justify-start pl-4">
+                            <Button variant="ghost" className="rounded-full border">Log-in</Button>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col items-center justify-center content-center w-full">
+                    <div>homepage</div>
+                </div>
+                <div className="flex flex-row items-center justify-center content-center w-full p-2">
+                    <div className="flex w-1/2 justify-end">
+                        <Button variant="ghost" className="h-5 p-1 hover:text-neutral">Therms of use</Button>
+                    </div>
+                    |
+                    <div className="flex w-1/2 justify-start">
+                        <Button variant="ghost" className="h-5 p-1">Privacy policy</Button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
 }
